81b8b58a1e7785d452e13fca981308dddec13ba2
sajibsaifullah <dusajibphy@gmail.com>
2023-04-02 11:07:29 +0600

    hero icons

--- src/compoments/Navbar/Navbar.jsx
+++ src/compoments/Navbar/Navbar.jsx
@@ -1,24 +1,32 @@
-import { list } from 'postcss';
-import React from 'react';
+import { list } from "postcss";
+import React, { useState } from "react";
+import Link from "../Link/Link";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
 
 const Navbar = () => {
-    const routes = [
-        { id: 1, name: 'Home', path: '/Home' },
-        { id: 2, name: 'About', path: '/about' },
-        { id: 3, name: 'Contact', path: '/contact' },
-        { id: 4, name: 'Products', path: '/products' },
-        { id: 5, name: 'Services', path: '/services' }
-    ];
-      
-    return (
-        <nav>
-            <ul>
-                {
-                    routes.map(route => <li>{route.name}</li>)
-                }
-            </ul>
-        </nav>
-    );
+  const [open, setOpen] = useState(false);
+
+  const routes = [
+    { id: 1, name: "Home", path: "/Home" },
+    { id: 2, name: "About", path: "/about" },
+    { id: 3, name: "Contact", path: "/contact" },
+    { id: 4, name: "Products", path: "/products" },
+    { id: 5, name: "Services", path: "/services" },
+  ];
+
+  return (
+    <nav>
+      <div onClick={() => setOpen(!open)}>
+        <span>{open === true ? "open" : "close"}</span>
+        <Bars3Icon className="h-6 w-6 text-purple-500" />
+      </div>
+      <ul className="md:flex">
+        {routes.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
